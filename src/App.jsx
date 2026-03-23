a6a6c063e856c45f173669428f8ec4b0740fa710
ratul-sardar <ratulsardar4747@gmail.com>
2026-03-24 01:24:26 +0600

    made a basic card component, but it needs more work

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Card from "./Components/Cards/Card";
+
 function App() {
   return (
     <>
       <div className="font-extrabold text-4xl">hi, im alive</div>
+      <h1 className="">heading</h1>
+      <Card></Card>
     </>
   );
 }
---
+++ src/Components/Cards/Card.jsx
@@ -0,0 +1,11 @@
+export default function Card() {
+  return (
+    <>
+      <div className="card card-md rounded-lg">
+        <div className="card-body">
+          <h4>title</h4>
+        </div>
+      </div>
+    </>
+  );
+}
