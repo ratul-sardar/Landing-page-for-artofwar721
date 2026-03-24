2ca29545a5e5d2aebb5d50630623f9f1411b9b65
ratul-sardar <ratulsardar4747@gmail.com>
2026-03-24 12:28:19 +0600

    null

--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card() {
   return (
     <>
-      <div className="card card-md rounded-lg">
+      <div className="card card-md rounded-lg border border-gray-400 shadow-md">
         <div className="card-body">
           <h4>title</h4>
         </div>
